f0db605f857bf5609e131a77c32c9b705caa85b6
denionline <danielximenes164@gmail.com>
2026-07-09 14:23:24 +0100

    feat: implement routing with createBrowserRouter and add authentication pages

---
+++ srcs/frontend/src/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+export const router = createBrowserRouter([
+	{ path: "/", element: <Navigate to="/login" replace /> },
+	{
+		element: <AuthLayout />,
+		children: [
+			{ path: "/login", element: <LoginPage /> },
+			{ path: "/register", element: <RegisterPage /> },
+		],
+	},
+	{path: "*", element: <NotFoundPage />}
+]);
