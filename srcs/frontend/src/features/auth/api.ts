38638aead81e2a0bb24b9e2183ef9b9c8a6d835b
denionline <danielximenes164@gmail.com>
2026-07-13 15:00:43 +0100

    feat: add mask api requests

--- srcs/frontend/src/features/auth/api.ts
+++ srcs/frontend/src/features/auth/api.ts
@@ -1,27 +1,24 @@
 import { http, HttpResponse } from "msw";
+import type { Credentials } from "./types";
 
-const users = {
-	artist: { id: "1", email: "artist@email.com", username: "artist", role: "artist" },
-	hirer: { id: "2", email: "hirer@email.com", username: "hirer", role: "hirer" },
-	admin: { id: "3", email: "admin@email.com", username: "admin", role: "admin" },
-};
+const users = [
+	{ id: "1", email: "artist@email.com", username: "artist", role: "artist" },
+	{ id: "2", email: "hirer@email.com", username: "hirer", role: "hirer" },
+	{ id: "3", email: "admin@email.com", username: "admin", role: "admin" },
+];
 
-export function register() {
+export function registerRequest() {}
 
-}
-
-export function login() {
+export function loginRequest(credentials: Credentials) {
 	http.get("/api/login", () => {
-		return HttpResponse.json(users.artist);
+		return HttpResponse.json(users.find((user) => user.email == credentials.email));
 	});
 }
 
 export function fetchMe() {
 	http.get("/api/me", () => {
-		return HttpResponse.json(users.artist);
-	})
+		// return HttpResponse.json(users.artist);
+	});
 }
 
-export function logout() {
-
-f}
+export function logout() {}
