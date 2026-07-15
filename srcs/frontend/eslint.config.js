bbe8f51478fa86f32bc5dc9c69e3572edc932913
denionline <danielximenes164@gmail.com>
2026-07-15 16:11:08 +0100

    feat: update eslint configuration to include 'dist' directory in ignores

--- srcs/frontend/eslint.config.js
+++ srcs/frontend/eslint.config.js
@@ -6,6 +6,7 @@ import prettier from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
 
 export default tseslint.config(
+	{ ignores: ["dist"] },
 	js.configs.recommended,
 	...tseslint.configs.recommended,
 	react.configs.flat.recommended,
