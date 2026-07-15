d16bb2d6a969ac62b51d0478729d44ade86fa8f6
denionline <danielximenes164@gmail.com>
2026-07-15 14:01:37 +0100

    refactor: remove unused navigation logic from DiscoverPage component

--- srcs/frontend/src/pages/DiscoverPage.tsx
+++ srcs/frontend/src/pages/DiscoverPage.tsx
@@ -1,13 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../features/auth/hooks/useAuth";
-
 export default function DiscoverPage() {
-	const { user } = useAuth();
-	const navigate = useNavigate();
-
-	console.log(user);
-	if (user?.role == "admin") {
-		navigate("/admin", { replace: true });
-	}
 	return <h1>Discover page</h1>;
 }
