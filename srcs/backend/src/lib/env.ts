5431a69be5f126a1d1dccd03c1aebdfb1c054248
gris06 <185072671+d-branco@users.noreply.github.com>
2026-07-23 11:12:55 +0100

    chore: added 42 credentials

--- srcs/backend/src/lib/env.ts
+++ srcs/backend/src/lib/env.ts
@@ -6,3 +6,7 @@ function required(name: string, value: string | undefined): string {
 
 export const SECRET = required("JWT_SECRET", process.env.JWT_SECRET);
 export const R_SECRET = required("JWT_REFRESH_SECRET", process.env.JWT_REFRESH_SECRET);
+
+export const FT_UID = required("FT_API_UID", process.env.FT_API_UID);
+export const FT_SECRET = required("FT_API_SECRET", process.env.FT_API_SECRET);
+export const FT_CALLBACK_URL = required("FT_API_CALLBACK_URL", process.env.FT_API_CALLBACK_URL);
