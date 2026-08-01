baf481c81ab3a4f6187b14ffab0c38edadf1e0be
gris06 <185072671+d-branco@users.noreply.github.com>
2026-08-01 08:27:48 +0100

    chore: update app ts

--- srcs/backend/src/app.ts
+++ srcs/backend/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./modules/auth/auth.routes.js";
 import usersRoutes from "./modules/users/users.routes.js";
+import gigsRoutes from "./modules/gigs/gigs.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -23,6 +24,7 @@ app.use("/api", apiRouter);
 
 apiRouter.use("/auth", authRoutes);
 apiRouter.use("/users", usersRoutes);
+apiRouter.use("/gigs", gigsRoutes);
 
 app.use(errorHandler);
 
