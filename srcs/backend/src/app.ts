6dbc62b04fa765a886ea0c739b227dd78d9640ed
gris06 <185072671+d-branco@users.noreply.github.com>
2026-08-06 23:58:13 +0100

    chore: import search routes

--- srcs/backend/src/app.ts
+++ srcs/backend/src/app.ts
@@ -6,6 +6,7 @@ import usersRoutes from "./modules/users/users.routes.js";
 import gigsRoutes from "./modules/gigs/gigs.routes.js";
 import swipesRoutes from "./modules/swipes/swipe.routes.js";
 import profileRoutes from "./modules/profile/profile.routes.js";
+import searchRoutes from "./modules/search/search.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
